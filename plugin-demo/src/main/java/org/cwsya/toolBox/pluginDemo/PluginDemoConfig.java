5634683c3f636a6dff7dfe0fa235d9b4c944cbf1
陈文生 <2771628927@qq.com>
2022-08-15 21:17:14 +0800

    feat(plugin-demo/,plugin-interface/): 定义插件开发规则
    向接口添加了一个规则并修改了实现类
    
    BREAKING CHANGE: 规范的定义
    
    Closes 解决插件接口规范问题

--- plugin-demo/src/main/java/org/cwsya/toolBox/pluginDemo/PluginDemoConfig.java
+++ plugin-demo/src/main/java/org/cwsya/toolBox/pluginDemo/PluginDemoConfig.java
@@ -2,8 +2,59 @@ package org.cwsya.toolBox.pluginDemo;
 
 import org.cwsya.toolBox.pluginInterface.PluginConfig;
 
+import javax.swing.*;
+
 /**
  * @author cws
  */
 public class PluginDemoConfig implements PluginConfig {
+    /**
+     * 获取插件主页面
+     *
+     * @return 插件主页面
+     */
+    @Override
+    public JFrame getIndex() {
+        return null;
+    }
+
+    /**
+     * 获取插件名称
+     *
+     * @return 插件名称
+     */
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    /**
+     * 获取插件作者
+     *
+     * @return 插件作者
+     */
+    @Override
+    public String getAuthor() {
+        return null;
+    }
+
+    /**
+     * 获取插件描述
+     *
+     * @return 插件描述
+     */
+    @Override
+    public String getDescribe() {
+        return null;
+    }
+
+    /**
+     * 获取插件相关网址
+     *
+     * @return url
+     */
+    @Override
+    public String getUrl() {
+        return null;
+    }
 }
--- plugin-interface/src/main/java/org/cwsya/toolBox/pluginInterface/PluginConfig.java
+++ plugin-interface/src/main/java/org/cwsya/toolBox/pluginInterface/PluginConfig.java
@@ -1,7 +1,39 @@
 package org.cwsya.toolBox.pluginInterface;
 
+import javax.swing.*;
+
 /**
+ * 插件配置接口
  * @author cws
  */
 public interface PluginConfig {
+    /**
+     * 获取插件主页面
+     * @return 插件主页面
+     */
+    JFrame getIndex();
+
+    /**
+     * 获取插件名称
+     * @return 插件名称
+     */
+    String getName();
+
+    /**
+     * 获取插件作者
+     * @return 插件作者
+     */
+    String getAuthor();
+
+    /**
+     * 获取插件描述
+     * @return 插件描述
+     */
+    String getDescribe();
+
+    /**
+     * 获取插件相关网址
+     * @return url
+     */
+    String getUrl();
 }
